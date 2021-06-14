82b26d043da48d0d93423680c8637165d1c52d3f
6395327574 <82360014+6395327574@users.noreply.github.com>
2021-06-14 16:38:47 +0530

    Add files via upload

---
+++ Java/Shekhar panwar-19051002/hi.java
@@ -0,0 +1,15 @@
+import java.lang. *;
+class hi
+{
+static int rollno=51;
+static String name ="shekhar";
+static void display( )
+{
+System.out.println("Rollno: " + rollno);
+System.out.println("Name : "+ name);
+}
+public static void main(String args[] )
+{
+display ( );
+}
+} 
